eac091a33f5559221a2507b5bd8fd2977a7fbf21
siraj <sirajulhaq3154@gmail.com>
2024-08-08 09:37:22 +0530

    Initial commit

---
+++ lib/CloneYT/bottomnavigaot.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/CloneYT/shortsscreen/utubeshortsMessi.dart';
+import 'package:youtube_clone/CloneYT/testutube.dart';
+import 'package:youtube_clone/CloneYT/youtubetabbar.dart';
+
+
+import 'home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: YoutubeNavi(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class YoutubeNavi extends StatefulWidget {
+  @override
+  State<YoutubeNavi> createState() => _YoutubeNaviState();
+}
+
+class _YoutubeNaviState extends State<YoutubeNavi> {
+  var screen = [YoutubeTabbar(), UtubeShortsMessi(), Testutube(),Testutube(),Testutube(),];
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: screen[index]),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.black, boxShadow: [
+          BoxShadow(
+              spreadRadius: 2,
+              blurRadius: 20,
+              color: Colors.black.withOpacity(0.3))
+        ]),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          currentIndex: index,
+          onTap: (tapindex) {
+            setState(() {
+              index = tapindex;
+            });
+          },
+          type: BottomNavigationBarType.shifting,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                label: "Home",
+                backgroundColor: Colors.black),
+            BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.play_rectangle_fill,
+                  color: Colors.white,
+                ),
+                label: "Shorts",
+                backgroundColor: Colors.black),BottomNavigationBarItem(
+                icon: Icon(
+                  CupertinoIcons.add_circled,
+                  color: Colors.white,
+                  size: 25,
+                ),
+                label: "",
+                backgroundColor: Colors.black),BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.subscriptions_outlined,
+                  color: Colors.white,
+                ),
+                label: "Subscription",
+                backgroundColor: Colors.black),BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                label: "Profile",
+                backgroundColor: Colors.black),
+          ],
+        ),
+      ),
+    );
+  }
+}
